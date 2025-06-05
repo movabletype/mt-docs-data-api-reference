8a45e841373b9e80473e3afd5c09d77642ecfcda
Masahiro Iuchi <miuchi@sixapart.com>
2025-06-05 09:27:46 +0900

    Update docs

--- redoc-build.sh
+++ redoc-build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-for v in v1 v2 v3 v4 v5 v6; do \
+for v in v1 v2 v3 v4 v5 v6 v7; do \
   npx @redocly/cli build-docs src/openapi/${v}.yaml \
     --template src/openapi/data-api-redoc.hbs \
     --disableGoogleFont \
